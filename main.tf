d10ba61bdd5783415f7f599e431569e0e49df3c4
Cody Green <cody@codygreen.com>
2019-10-23 21:50:10 -0500

    fix variable names

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ module "bigip_demo" {
   source  = "app.terraform.io/f5cloudsa/bigip-demo/aws"
   version = "0.1.1"
 
-  AccessKeyID     = "${var.bigip_demo_AccessKeyID}"
-  ec2_key_name    = "${var.bigip_demo_ec2_key_name}"
+  AccessKeyID     = "${AccessKeyID}"
+  ec2_key_name    = "${ec2_key_name}"
   region          = "us-east-2"
-  SecretAccessKey = "${var.bigip_demo_SecretAccessKey}"
+  SecretAccessKey = "${SecretAccessKey}"
 }
--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 //--------------------------------------------------------------------
 // Variables
-variable "bigip_demo_AccessKeyID" {}
-variable "bigip_demo_ec2_key_name" {}
-variable "bigip_demo_SecretAccessKey" {}
+variable "AccessKeyID" {}
+variable "ec2_key_name" {}
+variable "SecretAccessKey" {}
