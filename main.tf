794c9e80de67b4b42c0be0abc5a4e693b65ef026
Cody Green <cody@codygreen.com>
2019-10-23 21:52:22 -0500

    fix variable reference

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ module "bigip_demo" {
   source  = "app.terraform.io/f5cloudsa/bigip-demo/aws"
   version = "0.1.1"
 
-  AccessKeyID     = "${AccessKeyID}"
-  ec2_key_name    = "${ec2_key_name}"
+  AccessKeyID     = "${var.AccessKeyID}"
+  ec2_key_name    = "${var.ec2_key_name}"
   region          = "us-east-2"
-  SecretAccessKey = "${SecretAccessKey}"
+  SecretAccessKey = "${var.SecretAccessKey}"
 }
