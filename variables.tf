62530975f586fd35833b60fc09f3226a5fb37f65
Cody Green <cody@codygreen.com>
2019-10-23 21:39:03 -0500

    initial commit

---
+++ variables.tf
@@ -0,0 +1,5 @@
+//--------------------------------------------------------------------
+// Variables
+variable "bigip_demo_AccessKeyID" {}
+variable "bigip_demo_ec2_key_name" {}
+variable "bigip_demo_SecretAccessKey" {}
